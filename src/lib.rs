a6bb38af972dab3d6d728e0a8105bdc9a1c428b5
dalance <dalance@gmail.com>
2020-08-03 16:58:19 +0900

    Debug

--- src/lib.rs
+++ src/lib.rs
@@ -100,7 +100,10 @@ pub fn cc_version(tool: &Tool) -> Result<Version, Error> {
         Ok(Version::parse(version.trim())?)
     } else if tool.is_like_msvc() {
         dbg!(tool.to_command());
-        let ret = std::process::Command::new("cl")
+        //let ret = std::process::Command::new("cl")
+        //    .output()
+        //    .map_err(Error::from)?;
+        let ret = std::process::Command::new("cl.exe")
             .output()
             .map_err(Error::from)?;
         dbg!(&ret.stderr);
